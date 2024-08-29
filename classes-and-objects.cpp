05bd6e57f7bd5cb1bb04336f49ae25552b791613
erlynorbel <lebron_paulmark_dimayacyac@dlsl.edu.ph>
2024-08-29 09:59:35 -0400

    Second commit

--- classes-and-objects.cpp
+++ classes-and-objects.cpp
@@ -2,3 +2,23 @@
 
 using namespace std; 
 
+class Book {
+    private: 
+        string title; 
+        string author; 
+        int year; 
+    
+    public:
+        Book(string userTitle, string userAuthor, int userYear){
+            title = userTitle;
+            author = userAuthor;
+            year = userYear;
+        }
+
+        
+};
+
+
+int main(){
+    
+}
